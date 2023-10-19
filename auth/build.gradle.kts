1e9b8bab74bf77aa12abc4b4f083b56fac320740
achhuda <achmad.huda@visionplus.id>
2023-10-19 17:22:51 +0700

    update version

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -87,7 +87,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "id.visionplus"
             artifactId = "visionplus-bss-android"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate {
                 from(components["release"])
