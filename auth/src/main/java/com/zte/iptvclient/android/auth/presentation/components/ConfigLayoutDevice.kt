6bbeaac35b6c25d68edd30653c1035a87da2d092
achhuda <achmad.huda@visionplus.id>
2023-11-01 11:51:26 +0700

    config layout device mobile or tablet

---
+++ auth/src/main/java/com/zte/iptvclient/android/auth/presentation/components/ConfigLayoutDevice.kt
@@ -0,0 +1,83 @@
+package com.zte.iptvclient.android.auth.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.zte.iptvclient.android.auth.presentation.pages.ForgotPasswordPage
+import com.zte.iptvclient.android.auth.presentation.theme.ColorPrimary
+import com.zte.iptvclient.android.auth.presentation.theme.ColorTextSecondary
+import com.zte.iptvclient.android.auth.presentation.theme.VisionplusbssandroidTheme
+
+@Composable
+fun ConfigLayoutDevice(
+    screenMobile: @Composable () -> Unit,
+    screenTablet: @Composable () -> Unit
+) {
+    VisionplusbssandroidTheme {
+        val configuration = LocalConfiguration.current
+        val isTablet = configuration.screenWidthDp >= 840
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            if (isTablet) {
+                TabletLayout(screenTablet)
+            } else {
+                MobileLayout(screenMobile)
+            }
+        }
+    }
+}
+
+@Composable
+fun TabletLayout(screenTablet: @Composable () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            modifier = Modifier.weight(0.25f)
+        ) {
+        }
+        Box(
+            modifier = Modifier.weight(0.5f)
+        ) {
+            screenTablet()
+        }
+        Box(
+            modifier = Modifier.weight(0.25f)
+        ) {
+        }
+    }
+}
+
+@Composable
+fun MobileLayout(screenMobile: @Composable () -> Unit) {
+    screenMobile()
+}
+
+
+@Preview(
+    showBackground = true,
+    device = "id:pixel_c", widthDp = 960, heightDp = 600)
+@Composable
+fun ConfigLayoutDevicePreview() {
+    ConfigLayoutDevice(
+        screenMobile = {
+            Text("mobile",color = ColorTextSecondary)
+        },
+        screenTablet = {
+            Text("tablet",color = ColorTextSecondary)
+        }
+    )
+}
